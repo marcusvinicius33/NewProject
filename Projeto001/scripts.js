4bec842259f87175f61d09b70b52a06475d025a6
seu-username <vinicius.vigia@gmail.com>
2024-10-02 11:39:02 -0300

    atualizando

---
+++ Projeto001/scripts.js
@@ -0,0 +1,29 @@
+function trocarCor(cor){
+    document.body.style.backgroundColor = cor
+}
+
+function corAleatoria(){
+    const red = Math.floor(Math.random() * 255)
+    const green = Math.floor(Math.random() * 255)
+    const blue = Math.floor(Math.random() * 255)
+    
+    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+}
+
+function aplicarCorPersonalizada(){
+    const corInput = document.querySelector('.input-cor').value
+
+    trocarCor(corInput)
+}
+
+function escolherImagem(imagem){
+    const reader = new FileReader()
+
+    reader.onload = function(evento){
+        const urlImagem = evento.target.result
+
+        document.body.style.backgroundImage = `url('${urlImagem}')`
+    }
+
+    reader.readAsDataURL(imagem)
+}
