0e91a473d45bbaf605beea2652afb829973711f2
seu-username <vinicius.vigia@gmail.com>
2024-10-17 11:28:10 -0300

    atualizando

---
+++ Projeto006/scripts.js
@@ -0,0 +1,18 @@
+function adicionarCaracter(caracter){
+    const valorInput = document.querySelector('.display').value
+    
+    document.querySelector('.display').value = valorInput + caracter  
+}
+function limparTela(){
+    document.querySelector('.display').value = ''
+}
+function calcular(){
+    const valorInput = document.querySelector('.display').value
+
+    document.querySelector('.display').value = eval(valorInput)
+}
+function inverter(){
+    const valorInput = document.querySelector('.display').value
+
+    document.querySelector('.display').value = valorInput * -1
+}
